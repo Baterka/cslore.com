7914b114c7431b3192e99d8f0ccaf857e31d946e
Baterka <dave.bubenik@gmail.com>
2017-08-27 19:09:48 +0200

    Mysterybox Prize Generator
    Added source of 'Mysterybox Prize Generator'

---
+++ README.md
@@ -0,0 +1,38 @@
+# csgo.style - CS:GO Betting site
+
+This repository contains some public features of [csgo.style](https://www.csgo.style) gambling site.
+
+### Tech
+
+Project uses a number of open source projects to work properly:
+
+* [Twitter Bootstrap] - great UI boilerplate for modern web apps
+* [node.js] - evented I/O for the backend
+* [Express] - fast node.js network app framework [@tjholowaychuk]
+* [jQuery] - duh
+* and much more...
+
+### Scripts
+
+List of components we have decided to post are linked below.
+
+| Script | README |
+| ------ | ------ |
+| Mysterybox Prize Generator | [backend/nodejs/mysterybox/README.md] [CSmb] |
+
+### Todos
+
+ - Share more stuff
+
+License
+----
+
+MIT
+
+[node.js]: <http://nodejs.org>
+[Twitter Bootstrap]: <http://twitter.github.com/bootstrap/>
+[jQuery]: <http://jquery.com>
+[@tjholowaychuk]: <http://twitter.com/tjholowaychuk>
+[express]: <http://expressjs.com>
+
+[CSmb]: <https://github.com/Baterka/csgo.style/backend/nodejs/mysterybox/README.md>
---
+++ backend/nodejs/mysterybox/mysterybox_prize_generator.js
@@ -0,0 +1,95 @@
+var mb_prices = {
+	red: 250,
+	green: 500,
+	blue: 1000
+};
+var mb_bank = 0; //bank status
+
+var mb_nextwin = {
+	red: 0,
+	green: 0,
+	blue: 0
+};
+var mb_fee = 10; //%
+var mb_fees = 0; //lifetime fees
+
+function generatePrize(box_type) {
+	var fee = ((mb_nextwin[box_type] - mb_prices[box_type]) / 100) * mb_fee;
+	if (mb_bank <= -(mb_nextwin[box_type] + fee)) {
+		prize = mb_nextwin[box_type];
+		switch (box_type) {
+			case 'red':
+				var win = Random(251, 500);
+				if (Random(1, 4) == 1) { //25%
+					win = Random(501, 1000);
+					if (Random(1, 2) == 1) { //12.5%
+						win = Random(1001, 2500);
+						if (Random(1, 4) == 1) { //3.125%
+							win = Random(2501, 5000);
+							if (Random(1, 2) == 1) { //1.5625%
+								win = Random(5001, 10000);
+								if (Random(1, 3) == 1) { //0.521%
+									win = 20000;
+								}
+							}
+						}
+					}
+				}
+				break;
+			case 'green':
+				var win = Random(501, 1000);
+				if (Random(1, 4) == 1) { //25%
+					win = Random(1001, 2500);
+					if (Random(1, 2) == 1) { //12.5%
+						win = Random(2501, 5000);
+						if (Random(1, 4) == 1) { //3.125%
+							win = Random(5001, 10000);
+							if (Random(1, 2) == 1) { //1.5625%
+								win = Random(10001, 12500);
+								if (Random(1, 2) == 1) { //0.78125%
+									win = 20000;
+								}
+							}
+						}
+					}
+				}
+				break;
+			case 'blue':
+				var win = Random(1001, 2500);
+				if (Random(1, 4) == 1) { //25%
+					win = Random(2501, 5000);
+					if (Random(1, 2) == 1) { //12.5%
+						win = Random(5001, 10000);
+						if (Random(1, 4) == 1) { //3.125%
+							win = Random(10001, 15000);
+							if (Random(1, 2) == 1) { //1.5625%
+								win = 20000;
+							}
+						}
+					}
+				}
+				break;
+		}
+		mb_bank += mb_nextwin[box_type] + fee;
+		mb_nextwin[box_type] = win;
+		mb_fees += fee;
+	} else {
+		switch (box_type) {
+			case 'red':
+				prize = Random(50, 250);
+				break;
+			case 'green':
+				prize = Random(125, 500);
+				break;
+			case 'blue':
+				prize = Random(350, 1000);
+				break;
+		}
+		mb_bank -= mb_prices[box_type] - prize;
+	}
+	return prize;
+}
+
+function Random(bottom, top) {
+	return Math.floor(Math.random() * (1 + top - bottom)) + bottom;
+}
